321ad7a310251725cd5d357d9e7e85afd734867f
Luc <luc@aggett.com>
2023-07-06 13:21:32 +0200

    fix pos_tagging scripts

--- scripts/pos_tagging.py
+++ scripts/pos_tagging.py
@@ -1,5 +1,7 @@
 from transformers import AutoTokenizer, AutoModelForTokenClassification, pipeline
-from data import import_buenzli
+from data import import_buenzli, import_NOAH_sentences
+import json
+import tqdm
 
 model = AutoModelForTokenClassification.from_pretrained("noeminaepli/swiss_german_pos_model")
 tokenizer = AutoTokenizer.from_pretrained("noeminaepli/swiss_german_pos_model")
@@ -8,7 +10,7 @@ pos_tagger = pipeline('ner', model=model, tokenizer=tokenizer, aggregation_strat
 
 
 
-def pos_tag(text: str) -> list:
+def pos_tag(text: str) -> list[dict]:
     """
     Tags a text with part of speech tags
     :param text: the text to be tagged
@@ -16,9 +18,43 @@ def pos_tag(text: str) -> list:
     """
     return pos_tagger(text)
 
-def main():
+def tag_buenzli():
+    word_limit = 100
     # import the comments
-    comments = import_buenzli("data/buenzli/")
+    comments = import_buenzli("../scripts/comments.json")
     # iterate over the comments and tag them
-    for comment in comments:
-        comment["pos_tags"] = pos_tag(comment["body"])
+    print(f"applying POS tags to {word_limit if word_limit else len(comments)} words in buenzli corpus")
+    for comment in tqdm.tqdm(comments[:word_limit]):
+        pos_tags = []
+        for word in comment["body"].split():
+            word.lower().strip(".,!?;:()[]{}")
+            pos_tags.append(pos_tag(word))
+
+        comment["POS"] = pos_tags
+
+        for word in pos_tags:
+            for tag_dict in word:
+                for attr in tag_dict:
+                    if not isinstance(tag_dict[attr], str):
+                        tag_dict[attr] = str(tag_dict[attr])
+
+    # write the comments to a new json file
+    with open("comments_pos.json", "w") as f:
+        json.dump(comments[:word_limit], f, indent=4)
+
+def tag_NOAH():
+    # import the sentences
+    sentences = import_NOAH_sentences("../NOAH-Corpus")
+    # iterate over the sentences and tag them
+    tagged_sentences = []
+    for sentence in tqdm.tqdm(sentences):
+        tagged_sentences.append(pos_tag(sentence))
+
+    # write the sentences to a new file
+    with open("NOAH_sentences_pos.csv", "w") as f:
+        for sentence in tagged_sentences:
+            f.write(sentence + "\n")
+
+
+if __name__ == "__main__":
+    tag_buenzli()
